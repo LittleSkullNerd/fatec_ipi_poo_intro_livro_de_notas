40227410b6e8cfade23dadbb52cada33383f04b8
LuLu <lulu.sonoda@hotmail.com>
2023-08-31 18:02:43 -0300

    ap06(java): teste do livro de notas

--- TesteLivroDeNotas.java
+++ TesteLivroDeNotas.java
@@ -1,6 +1,9 @@
 public class TesteLivroDeNotas{
     static public void main(String args []){
         //variavel de referencia, parecido com ponteiro da linguagem C, mas JVM nao tem aritmetica de ponteiro
+        //malloc
         LivroDeNotas livroDeNotas = new LivroDeNotas(); //tipo, variavel - new-> operador de criação de objetos
+        livroDeNotas.exibirMensagem();
+        System.out.println("Ate logo");
     }
 }
