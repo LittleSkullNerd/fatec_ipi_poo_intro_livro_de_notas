bec1d63d3ec0b3bbb5ba94710981d07ef2c4b5d9
LuLu <lulu.sonoda@hotmail.com>
2023-09-14 16:07:12 -0300

    ap06(java): fim da listagem 12.1

--- LivroDeNotas.java
+++ LivroDeNotas.java
@@ -1,5 +1,18 @@
 public class LivroDeNotas{
+    //variavel de instancia
+    //instancia é sinonimo de objeto 
+    String nomeDisciplina;
+
     public void exibirMensagem (){
-        System.out.println("Bem vindo ao livro de notas");
+        System.out.printf("Bem vindo ao livro de notas %s\n ", nomeDisciplina);
     } 
+
+
+//construtor padrão (aquele cuja lista de parametros e vazia)
+LivroDeNotas(String nomeDisciplina){
+    //shadowing
+    this.nomeDisciplina = nomeDisciplina;
+
+
+    }
 }
--- TesteLivroDeNotas.java
+++ TesteLivroDeNotas.java
@@ -1,8 +1,17 @@
 public class TesteLivroDeNotas{
     static public void main(String args []){
+
+//nome completamente qualificado da classe
+String nomeDisciplina = javax.swing.JOptionPane.showInputDialog("Digite o nome da disciplina");
+
         //variavel de referencia, parecido com ponteiro da linguagem C, mas JVM nao tem aritmetica de ponteiro
         //malloc
-        LivroDeNotas livroDeNotas = new LivroDeNotas(); //tipo, variavel - new-> operador de criação de objetos
+
+        // interferencia de tipo "var"
+        //var: somente a partir do Java 10
+        var livroDeNotas = new LivroDeNotas(nomeDisciplina); //tipo, variavel - new-> operador de criação de objetos
+        //livroDeNotas.nomeDisciplina = nomeDisciplina;
+        //enviando a mensagem "exibirMensagem" ao objeto livro de notas"
         livroDeNotas.exibirMensagem();
         System.out.println("Ate logo");
     }
